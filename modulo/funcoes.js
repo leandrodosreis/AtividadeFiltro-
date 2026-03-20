bb76b01f0c1cc2a7176cc37b59ac7980a27ad973
LEANDRO DOS REIS FILHO <25203700@CFP127ALUNO.LOCAL>
2026-03-20 17:03:25 -0300

    fazendo getEstadoRegiao

--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -2,19 +2,119 @@ const dadosUtilizados = require('./estados_cidades')
 
 // let minhaLista = dadosUtilizados.listaDeEstados
 
-function getListaDeEstados(){
+const dados  = dadosUtilizados
 
-    let uf = 'sigla'
+const lista = []
 
-    dadosUtilizados.listaDeEstados.estados.forEach(function(itemEstado){
+// function getListaDeEstados(){ 
+    
+//     let conteudo
+//     let resposta = []
 
-        itemEstado.sigla.forEach(function(itemSigla){
-            if(uf == itemSigla)
-                console.log(itemEstado)
-        })
-        
+//     dados.listaDeEstados.estados.forEach(function(itemEstado){
+//         resposta.push (itemEstado.sigla)
+//     })
+
+//     conteudo = {
+//             "uf" : resposta,
+//             "quantidade" : resposta.length
+//         }
+//     return conteudo
+
+// }
+
+// console.log(getListaDeEstados())
+
+function getListaDeEstados(){ 
+    
+    let conteudo
+    let resposta = []
+
+    dados.listaDeEstados.estados.forEach(function(itemEstado){
+        resposta.push (itemEstado.sigla)
+    })
+
+    conteudo = {
+            "uf" : resposta,
+            "quantidade" : resposta.length
+        }
+    return conteudo
+    
+}
+
+function getDadosEstado(siglas){
+
+    let conteudo = {
+        "uf" : null ,
+        "descricao" : null ,
+        "capital" : null ,
+        "regiao" : null
+    } 
+
+    dados.listaDeEstados.estados.forEach(function(itemEstado){
+
+        if(itemEstado.sigla == siglas){
+            conteudo.uf = itemEstado.sigla
+            conteudo.descricao = itemEstado.nome
+            conteudo.capital = itemEstado.capital
+            conteudo.regiao = itemEstado.regiao
+        }
+
+    })
+
+    if(conteudo.uf == null 
+        || conteudo.descricao == null 
+        || conteudo.capital == null
+        || conteudo.regiao == null){
+        return false
+
+    }else{  return conteudo }
+
+}
+
+function getCapitalEstado(siglas){
+
+    let conteudo = {
+        "uf" : null ,
+        "descricao" : null ,
+        "capital" : null 
+    }
+
+    dados.listaDeEstados.estados.forEach(function(itemEstado){
+        if(itemEstado.sigla == siglas){
+            conteudo.uf = itemEstado.sigla
+            conteudo.descricao = itemEstado.nome
+            conteudo.capital = itemEstado.capital
+        }
+    })
+
+    if(conteudo.uf == null 
+        || conteudo.descricao == null 
+        || conteudo.capital == null){
+        return false
+
+    }else{  return conteudo }
+
+}
+
+function getEstadoRegiao(regioes){
+
+    let conteudo = {
+        "regiao" : null ,
+        "estados" : null
+    }
+
+    list = []
+
+    dados.listaDeEstados.estados.forEach(function(itemEstado){
+        if(regioes == itemEstado.regiao){
+            conteudo.estados = itemEstado
+        }
     })
 
 }
 
-getListaDeEstados()
+console.log(getEstadoRegiao("SUL"))
+// console.log(getCapitalEstado('SP'))
+// console.log(getDadosEstado('SP'))
+// console.log(getListaDeEstados())
